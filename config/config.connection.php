2e751fe2b470c91c62d3f2d558432b8ab1e846a9
lightwolf37 <149528778+LightWolf11@users.noreply.github.com>
2026-03-30 12:24:14 +0300

    created cfg db

---
+++ config/config.connection.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'name' => 'hospital_site',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ],
+    'app' => [
+        'base_url' => 'http://hospitalsite',
+        'public_path' => '',
+        'session_name' => 'HOSPITAL_SESSID',
+    ],
+    'upload' => [
+        'max_bytes' => 5 * 1024 * 1024,
+        'allowed_mime' => ['image/jpeg', 'image/png', 'image/webp', 'image/gif', 'application/pdf'],
+    ],
+];
