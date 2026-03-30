eb23c257adbb92a3f9d680920670177c2a3856b6
lightwolf37 <149528778+LightWolf11@users.noreply.github.com>
2026-03-30 12:24:23 +0300

    created cfg mail

---
+++ config/config.mail.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'mail' => [
+        'from_email' => 'example@gmail.com',
+        'from_name' => 'Больница «В последний путь»',
+        'smtp_host' => 'smtp.gmail.com',
+        'smtp_port' => 587,
+        'smtp_user' => 'example@gmail.com',
+        'smtp_pass' => 'YOUR_PASSWORD',
+        'smtp_secure' => 'tls',
+        'smtp_debug' => 0,
+        'smtp_relaxed_ssl' => false,
+    ],
+];
